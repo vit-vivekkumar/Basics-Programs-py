a8e46bbedba3478f3fba0757054fcfa3f73ca82c
vivek Vishwakarma <90178572+vit-vivekkumar@users.noreply.github.com>
2022-11-30 18:01:08 +0000

    lambda addtion

--- add.py
+++ add.py
@@ -1,1 +1,11 @@
-print('vivek')
+#Addition of two numbers
+'''
+1. This code uses a lambda function to add two numbers.
+2. The lambda function takes in two arguments: x and y.
+3. The lambda function returns the sum of x and y.
+4. The code then prints out the sum of 10 and 43.
+
+'''
+sum=lambda x,y:x+y
+
+print(sum(10,43))
