4ce52f41f22250665a03e01aa760ff565401a25e
vivek Vishwakarma <90178572+vit-vivekkumar@users.noreply.github.com>
2022-11-30 18:20:59 +0000

    changes

--- Basics/addition.py
+++ Basics/addition.py
@@ -1,1 +1,14 @@
-print('fd')
+#Addition of two numbers
+'''
+1. This code uses a lambda function to add two numbers.
+2. The lambda function takes in two arguments: x and y.
+3. The lambda function returns the sum of x and y.
+4. The code then prints out the sum of 10 and 43.
+
+'''
+sum=lambda x,y:x+y
+print(sum(10,43))
+
+# x=30
+# y=5
+# print(x if x>y else y)
